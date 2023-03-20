ae86211a92c831af9ff3903343f0357e03de35a9
andre.carvalho <andrelcalves@gmail.com>
2023-03-20 17:09:55 -0300

    Project created

---
+++ src/main/java/com/radix/api/RadixTranningJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.radix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadixTranningJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadixTranningJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/radix/api/RadixTranningJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadixTranningJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
