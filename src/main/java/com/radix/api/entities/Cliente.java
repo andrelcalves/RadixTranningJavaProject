ce1ba80550a4404a3d4ef503cb1eac5741b25e11
Andre Luis Carvalho Alves <andrelcalves@gmail.com>
2023-03-21 15:58:14 -0300

    Criação da entity de clientes

---
+++ src/main/java/com/radix/api/entities/Cliente.java
@@ -0,0 +1,68 @@
+package com.radix.api.entities;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name="cliente")
+public class Cliente{
+	
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+	//private List<Pedidos> pedidos;
+
+	public Cliente() {}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "razaoSocial", nullable = false)
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@Column(name = "cnpj", nullable = false)
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Column(name = "dataCriacao", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name = "dataAtualizacao", nullable = false)
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+
+		
+		
+}
